400f581d07f95c7fe90f9b5ddfced5a90850bb8a
nancyalvarez <nancy.alvarez@ceiba.com.co>
2020-12-16 17:33:00 -0500

    Se elimina informacion de ejemplo de usuario

--- microservicio/src/main/resources/db/migration/DDL/V1.0__schema.sql
+++ microservicio/src/main/resources/db/migration/DDL/V1.0__schema.sql
@@ -1,11 +1,3 @@
-create table usuario (
- id int(11) not null auto_increment,
- nombre varchar(100) not null,
- clave varchar(45) not null,
- fecha_creacion datetime null,
- primary key (id)
-);
-
 create table servicio_estetico(
     id int(11) not null auto_increment,
     ise varchar(12) not null,
